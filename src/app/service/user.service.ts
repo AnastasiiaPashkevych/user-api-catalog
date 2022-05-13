b78a8189d1a6c8780e6189fff435bae763bf2e28
Anastasia <nastyapashkevich10@gmail.com>
2022-05-13 20:44:02 +0200

    mapping users

--- src/app/service/user.service.ts
+++ src/app/service/user.service.ts
@@ -1,6 +1,7 @@
 import {Injectable} from '@angular/core';
 import {HttpClient} from "@angular/common/http";
-import {Observable} from "rxjs";
+import {map, Observable} from "rxjs";
+import {User} from "../interface/user.interface";
 
 @Injectable({
   providedIn: 'root'
@@ -13,9 +14,39 @@ export class UserService {
 
   getUsers(size: number = 10): Observable<any> {
     return this.http.get<any>(`${this.apiUrl}/?results=${size}`)
+      .pipe(
+        map(response => this.processResponse(response))
+      )
   }
 
   getUser(uuid: number = 1): Observable<any> {
     return this.http.get<any>(`${this.apiUrl}/?uuid=${uuid}`)
+      .pipe(
+        map(response => this.processResponse(response))
+      )
+  }
+
+  private processResponse(response: Response): Response {
+    return {
+      // @ts-ignore
+      info: {...response.info},
+      // @ts-ignore
+      results: response.results.map((user: any) => (<User>{
+        uuid: user.login.uuid,
+        firstName: user.name.first,
+        lastName: user.name.last,
+        email: user.email,
+        username: user.login.username,
+        gender: user.gender,
+        address: `${user.location.street.number} ${user.location.street.name} ${user.location.street.city}, ${user.location.street.country}`,
+        dateOfBirth: user.dob.date,
+        phone: user.phone,
+        imageUrl: user.picture.medium,
+        coordinate: {
+          latitude: +user.location.coordinates.latitude,
+          longitude: +user.location.coordinates.longitude
+        }
+      }))
+    }
   }
 }
