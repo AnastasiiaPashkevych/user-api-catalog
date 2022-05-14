a7847e1fbbf5e32ab6d4f80e9031ac8dfc2c8cc0
Anastasia <nastyapashkevich10@gmail.com>
2022-05-14 21:49:02 +0200

    displaying user detail

--- src/app/component/user-detail/user-detail.component.ts
+++ src/app/component/user-detail/user-detail.component.ts
@@ -1,6 +1,7 @@
 import {Component, OnInit} from '@angular/core';
 import {ActivatedRoute, ParamMap} from "@angular/router";
 import {UserService} from "../../service/user.service";
+import {Response} from "../../interface/response.interface";
 
 @Component({
   selector: 'app-user-detail',
@@ -8,6 +9,7 @@ import {UserService} from "../../service/user.service";
   styleUrls: ['./user-detail.component.css']
 })
 export class UserDetailComponent implements OnInit {
+  response: Response
 
   constructor(private activatedRoute: ActivatedRoute, private userService: UserService) {
   }
@@ -18,6 +20,7 @@ export class UserDetailComponent implements OnInit {
         this.userService.getUser(params.get('uuid')!)
           .subscribe((response: any) => {
             console.log(response)
+            this.response = response
           })
       })
   }
